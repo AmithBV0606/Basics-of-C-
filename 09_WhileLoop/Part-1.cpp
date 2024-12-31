a23bfa5d1855f6f7302ff0e7d1ede2943e032fb5
AmithBV0606 <amithrao0606@gmail.com>
2024-12-31 08:27:25 +0530

    Studied about while and do-while loop.

---
+++ 09_WhileLoop/Part-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    while (i <= 5)
+    {
+        cout << "Amith " << i << endl;
+        i = i + 1;
+    }
+    return 0;
+}
---
+++ 09_WhileLoop/Part-2.cpp
@@ -0,0 +1,17 @@
+// do-while loop
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    do
+    {
+        cout << "Amith " << i << endl;
+        i = i + 1;
+    } while (i <= 5);
+
+    return 0;
+}
